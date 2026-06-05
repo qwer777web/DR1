1a614afb2429d6bf0ab629edae4d3686cb5935ba
qwer777web <zzzwei666666@gmail.com>
2026-06-05 11:02:33 +0800

    Add backend/dr1-system/src/main/java/com/dr1/system/service/SysUserService.java

---
+++ backend/dr1-system/src/main/java/com/dr1/system/service/SysUserService.java
@@ -0,0 +1,8 @@
+package com.dr1.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dr1.system.entity.SysUser;
+
+public interface SysUserService extends IService<SysUser> {
+}
+
