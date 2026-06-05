10c2b4686cb43c918b1da49069e187dc62be70c9
qwer777web <zzzwei666666@gmail.com>
2026-06-05 11:03:03 +0800

    Add dr1-ui/src/main.js

---
+++ dr1-ui/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import './assets/styles.css'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
+
+Object.entries(ElementPlusIconsVue).forEach(([key, component]) => {
+  app.component(key, component)
+})
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
+
