7cf0a48ce162b83cd135de50f7d37262b59e353c
qwer777web <zzzwei666666@gmail.com>
2026-06-05 11:01:58 +0800

    Add backend/dr1-common/src/main/java/com/dr1/common/core/PageResult.java

---
+++ backend/dr1-common/src/main/java/com/dr1/common/core/PageResult.java
@@ -0,0 +1,57 @@
+package com.dr1.common.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PageResult<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<T> records;
+    private Long total;
+    private Long current;
+    private Long size;
+
+    public PageResult() {
+    }
+
+    public PageResult(List<T> records, Long total, Long current, Long size) {
+        this.records = records;
+        this.total = total;
+        this.current = current;
+        this.size = size;
+    }
+
+    public List<T> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<T> records) {
+        this.records = records;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Long getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Long current) {
+        this.current = current;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+}
+
