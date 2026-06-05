166da2e58c1e33074c8ecba87b012a3720d8545b
qwer777web <zzzwei666666@gmail.com>
2026-06-05 11:02:31 +0800

    Add backend/dr1-system/src/main/java/com/dr1/system/mapper/SysUserMapper.java

---
+++ backend/dr1-system/src/main/java/com/dr1/system/mapper/SysUserMapper.java
@@ -0,0 +1,8 @@
+package com.dr1.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dr1.system.entity.SysUser;
+
+public interface SysUserMapper extends BaseMapper<SysUser> {
+}
+
