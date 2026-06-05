8d5383d740d595ed6db0e2dd15a7dbdfcafd1896
qwer777web <zzzwei666666@gmail.com>
2026-06-05 11:02:54 +0800

    Add dr1-ui/src/api/auth.js

---
+++ dr1-ui/src/api/auth.js
@@ -0,0 +1,10 @@
+import request from './request'
+
+export function login(data) {
+  return request.post('/auth/login', data)
+}
+
+export function getCurrentUser() {
+  return request.get('/auth/me')
+}
+
